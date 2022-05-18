82bdd1a8c9cc3f00f02c5aa15aecd1d31683df77
rupak-nm <rupak@neptunemutual.com>
2022-05-18 19:33:09 +0545

    fixed tooltip on mobile & made responsive

--- components/common/Tooltip/index.tsx
+++ components/common/Tooltip/index.tsx
@@ -5,15 +5,17 @@ import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 interface TooltipProps {
   text: string;
   position?: "bottom" | "left" | "right" | "top";
+  open?: boolean;
 }
 
 export const Tooltip: FC<TooltipProps> = ({
   text,
   children,
   position = "right",
+  open,
 }) => {
   return (
-    <TooltipPrimitive.Root delayDuration={0}>
+    <TooltipPrimitive.Root delayDuration={0} open={open}>
       <TooltipPrimitive.Trigger>
         <span className="sr-only">Tooltip Button</span>
         {children}
